7863e3dd49c8f8170770ff26b290cf1740e109f4
long <nguyenbaolong150405@gmail.com>
2021-10-23 14:38:58 +0700

    ngu

---
+++ javascript/lop10.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+        showSlides(slideIndex);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
+
+
+
+
+
+// --------------------------------------------------------
+var slideIndex2 = 1;
+    showSlides2(slideIndex2);
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+}
+function showSlides2(n) {
+    var i;
+    var slides2 = document.getElementsByClassName("mySlides-2");
+    if (n > slides2.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides2.length}
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
+    }
+    slides2[slideIndex2-1].style.display = "block";
+}
